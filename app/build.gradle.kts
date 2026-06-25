6ecf749c7d2b7a5fd85b47dbfe1477cf0cf82d8c
sunaan.sultan <sunaansultaniit@gmail.com>
2026-06-25 07:29:58 +0600

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.orbit.starsystems"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.6"
+        versionCode = 17
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
